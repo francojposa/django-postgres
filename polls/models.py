83bedd3d2e6a8be4ce53ce02df21fdd669df2568
francojposa <franco@francoposa.io>
2020-08-11 22:13:15 -0500

    Up to writing first Django app, part 2. Using Uvicorn ASGI server, Postgres, UUID primary keys, and WhiteNoiseMiddleware for static file serving

---
+++ polls/models.py
@@ -0,0 +1,26 @@
+import uuid
+
+from django.db import models
+
+
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.choice_text
