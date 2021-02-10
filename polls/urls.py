807eab28ffe3b562e537e15a0635bccd942346fb
francoposa <franco@francoposa.io>
2021-02-09 19:07:10 -0600

    adding some polls routes, adding ipython as dep

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Ex. /polls/
     path("", views.index, name="index"),
+    # Ex./polls/6722b747-f497-4095-95bf-7ba4402ba3b8/
+    path("<str:question_id>/", views.detail, name="detail"),
+    # Ex./polls/6722b747-f497-4095-95bf-7ba4402ba3b8/results
+    path("<str:question_id>/results/", views.results, name="results"),
+    # Ex./polls/6722b747-f497-4095-95bf-7ba4402ba3b8/vote
+    path("<str:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,18 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request: HttpRequest, question_id: str) -> HttpResponse:
+    response = f"You're looking at question {question_id}"
+    return HttpResponse(response)
+
+
+def results(request: HttpRequest, question_id: str) -> HttpResponse:
+    response = f"You're looking at results for question {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request: HttpRequest, question_id: str) -> HttpResponse:
+    response = f"You're voting on question {question_id}"
+    return HttpResponse(response)
